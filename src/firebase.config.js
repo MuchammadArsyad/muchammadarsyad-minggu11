0baec3ab8ff547ca2577bbb17ba332b8288cfefc
Muchammad Arsyad <arsyad1822@gmail.com>
2020-04-15 16:53:21 +0700

    update

---
+++ src/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBvAe91aFzDaUtdEnHMS7Axlb-UCaPl3bA",
+  authDomain: "pbfminggu11-55ffa.firebaseapp.com",
+  databaseURL: "https://pbfminggu11-55ffa.firebaseio.com",
+  projectId: "pbfminggu11-55ffa",
+  storageBucket: "pbfminggu11-55ffa.appspot.com",
+  messagingSenderId: "899172473882",
+  appId: "1:899172473882:web:afbcd39cae9dd941ec45a3",
+  measurementId: "G-FJZY5NWBV1"
+};
